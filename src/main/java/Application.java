4470eaf839274633761acbeba3d4d3e4ae4afa2e
Björn Hegstam <bjorn.hegstam@gmail.com>
2017-06-05 14:28:21 +0200

    Rename main class

---
+++ src/main/java/Application.java
@@ -0,0 +1,15 @@
+import static spark.Spark.*;
+
+public class Application {
+    public static void main(String[] args) {
+        // Configure spark
+        port(4567);
+
+        staticFiles.location("/public");
+        staticFiles.expireTime(600);
+
+        get("/hello", (req, res) -> "Hello World!");
+        // TODO: Organize routes better
+        // TODO: Get basic administration site up and running with in memory storage
+    }
+}
