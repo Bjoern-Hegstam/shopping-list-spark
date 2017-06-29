751c2c1e5cf26690e6d03ddf3206747c64855ce2
Björn Hegstam <bjorn.hegstam@gmail.com>
2017-06-29 21:08:56 +0200

    Use flyway for db migration

--- migrate_and_generate_sources.py
+++ migrate_and_generate_sources.py
@@ -15,14 +15,22 @@ def main():
     db_user = data['database']['user']
     db_password = data['database']['password']
 
-    # Generate sources from database
     subprocess.call([
         'mvn',
+        'flyway:migrate',
         '-Ddb.url={}'.format(db_url),
         '-Ddb.user={}'.format(db_user),
         '-Ddb.password={}'.format(db_password),
-        'generate-sources'
     ], shell=True)
+
+    subprocess.call([
+        'mvn',
+        'generate-sources',
+        '-Ddb.url={}'.format(db_url),
+        '-Ddb.user={}'.format(db_user),
+        '-Ddb.password={}'.format(db_password)
+    ], shell=True)
+    # Generate sources from database
 
 
 if __name__ == '__main__':
