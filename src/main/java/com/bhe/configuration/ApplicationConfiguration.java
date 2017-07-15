bed91e07a54505b06c3f04329234d0aa01ac9b74
Björn Hegstam <bjorn.hegstam@gmail.com>
2017-07-15 11:41:23 +0200

    Remove boilerplate in configuration classes

--- src/main/java/com/bhe/configuration/ApplicationConfiguration.java
+++ src/main/java/com/bhe/configuration/ApplicationConfiguration.java
@@ -1,18 +1,9 @@
 package com.bhe.configuration;
 
 import com.bhe.configuration.property.Database;
+import lombok.Data;
 
+@Data
 public class ApplicationConfiguration {
     private Database database;
-
-    public ApplicationConfiguration() {
-    }
-
-    public Database getDatabase() {
-        return database;
-    }
-
-    public void setDatabase(Database database) {
-        this.database = database;
-    }
 }
