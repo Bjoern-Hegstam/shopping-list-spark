ed6a2d07ec3796e4a95f4e5bdc992fef916e74f3
Björn Hegstam <bjorn.hegstam@gmail.com>
2017-10-01 11:03:24 +0200

    Extract ApplicationBase

--- src/main/java/com/bhe/Application.java
+++ src/main/java/com/bhe/Application.java
@@ -7,24 +7,17 @@ import com.bhe.user.UserAdministrationController;
 import com.bhe.user.UserModule;
 import com.bhe.user.UserRegistrationController;
 import com.bhe.user.api.UserApiController;
-import com.bhe.util.Filters;
-import com.bhe.util.webapp.Controller;
 import com.google.inject.Guice;
 import com.google.inject.Inject;
 import com.google.inject.Injector;
 import spark.Service;
 
-import java.util.List;
-
 import static java.util.Arrays.asList;
 import static java.util.Collections.singletonList;
-import static spark.Service.ignite;
 
-public class Application {
+public class Application extends ApplicationBase {
 
     private final ApplicationConfiguration configuration;
-    private final List<Controller> appControllers;
-    private final List<Controller> apiControllers;
 
     public static void main(String[] args) {
         Injector injector = Guice.createInjector(
@@ -46,39 +39,27 @@ public class Application {
             UserAdministrationController userAdministrationController,
             UserApiController userApiController
     ) {
+        super(
+                asList(
+                        indexController,
+                        loginController,
+                        userRegistrationController,
+                        userAdministrationController
+                ),
+                singletonList(
+                        userApiController
+                )
+        );
+
         this.configuration = configuration;
-
-        appControllers = asList(
-                indexController,
-                loginController,
-                userRegistrationController,
-                userAdministrationController
-        );
-
-        apiControllers = singletonList(
-                userApiController
-        );
     }
 
-    private void init() {
-        Service http = ignite();
-        configureServer(http);
-        configureRoutes(http);
-    }
-
-    private void configureServer(Service http) {
+    @Override
+    protected void configureServer(Service http) {
         http.port(configuration.getServer().getPort());
 
         http.staticFiles.location("/public");
         http.staticFiles.expireTime(600);
     }
 
-    private void configureRoutes(Service http) {
-        appControllers.forEach(c -> c.configureRoutes(http));
-
-        http.path("/api", () -> {
-            http.before("/*", Filters::userIsLoggedIn);
-            apiControllers.forEach(c -> c.configureRoutes(http));
-        });
-    }
 }
