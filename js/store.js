4f6ad98c5fa11efd29c8be4322062b31b1d36bee
Björn Hegstam <bjorn.hegstam@gmail.com>
2018-07-15 21:58:58 +0200

    Bugfix: Store user information to application store

--- js/store.js
+++ js/store.js
@@ -3,20 +3,20 @@ import axiosMiddleware from "redux-axios-middleware";
 import reducers from "./reducers";
 import axios from "axios/index";
 
-const STATE_KEY = 'STATE';
+const STATE_KEY = 'SHOPPING_LIST_STATE';
 
 function loadState() {
     const persistedState = localStorage.getItem(STATE_KEY);
     if (persistedState) {
         return {
-            user: JSON.parse(persistedState)
+            auth: JSON.parse(persistedState)
         };
     }
     return {};
 }
 
 function saveState(store) {
-    localStorage.setItem(STATE_KEY, JSON.stringify(store.getState().user));
+    localStorage.setItem(STATE_KEY, JSON.stringify(store.getState().auth));
 }
 
 export default () => {
