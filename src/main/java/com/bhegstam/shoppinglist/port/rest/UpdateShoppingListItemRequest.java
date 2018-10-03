e06ce370d642aed4b62f39112ac62e138c2811ba
Björn Hegstam <bjorn.hegstam@gmail.com>
2018-10-03 18:35:28 +0200

    Add missing toString for request object

--- src/main/java/com/bhegstam/shoppinglist/port/rest/UpdateShoppingListItemRequest.java
+++ src/main/java/com/bhegstam/shoppinglist/port/rest/UpdateShoppingListItemRequest.java
@@ -3,6 +3,8 @@ package com.bhegstam.shoppinglist.port.rest;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
+import org.apache.commons.lang.builder.ToStringBuilder;
+import org.apache.commons.lang.builder.ToStringStyle;
 
 @Getter
 public class UpdateShoppingListItemRequest {
@@ -16,5 +18,10 @@ public class UpdateShoppingListItemRequest {
     ) {
         this.quantity = quantity;
         this.inCart = inCart;
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.SHORT_PREFIX_STYLE);
     }
 }
