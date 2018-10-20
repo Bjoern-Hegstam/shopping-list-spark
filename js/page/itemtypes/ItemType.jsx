4ea6f51a36d076510b13ae547d356f39e81e8616
Björn Hegstam <bjorn.hegstam@gmail.com>
2018-10-20 17:02:44 +0200

    Add button for deleting item type

--- js/page/itemtypes/ItemType.jsx
+++ js/page/itemtypes/ItemType.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { FaTrash } from 'react-icons/fa';
 
 export default class ItemType extends React.Component {
     static propTypes = {
         id: PropTypes.string.isRequired,
         name: PropTypes.string.isRequired,
+
+        onDelete: PropTypes.func,
+    };
+
+    static defaultProps = {
+        onDelete: undefined,
+    };
+
+    handleOnDeleteClicked = () => {
+        const { onDelete, id } = this.props;
+
+        if (onDelete) {
+            onDelete(id);
+        }
     };
 
     render() {
@@ -12,6 +27,13 @@ export default class ItemType extends React.Component {
 
         return (
             <div className="item-type">
+                <button
+                    type="button"
+                    className="item-type__delete-button"
+                    onClick={this.handleOnDeleteClicked}
+                >
+                    <FaTrash />
+                </button>
                 <div className="item-type__name">{name}</div>
             </div>
         );
