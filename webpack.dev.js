7d58e95d5fa7d874f6a2e9d5d799fa64475b981d
Björn Hegstam <bjorn.hegstam@gmail.com>
2019-03-16 11:48:40 +0100

    Don't actually need to use webpack dev server proxy

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,12 +4,4 @@ const common = require('./webpack.common.js');
 module.exports = merge(common, {
     mode: 'development',
     devtool: 'inline-source-map',
-    devServer: {
-        proxy: {
-            '/api': {
-                target: 'http://localhost:8080',
-                pathRewrite: { '^/api': '' },
-            },
-        },
-    },
 });
