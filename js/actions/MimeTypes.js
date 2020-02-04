1ddb79aebb4e71ce78382c0e230086110f3b375e
Björn Hegstam <bjorn.hegstam@gmail.com>
2020-02-04 20:50:55 +0100

    Patch frontend dependencies

--- js/actions/MimeTypes.js
+++ js/actions/MimeTypes.js
@@ -1,2 +1,2 @@
 export const AUTH_1_0 = 'application/com.bhegstam.shoppinglist.auth-1.0+json';
-export const SHOPPING_LIST_1_0 = 'application/com.bhegstam.shoppinglist.shopping-list-1.1+json';
+export const SHOPPING_LIST_1_0 = 'application/com.bhegstam.shoppinglist.shopping-list-1.0+json';
